61129eed951720bd075cad27f9b85991b46bab53
swiftcode <mpc@junct.com>
2022-05-19 10:14:00 -0500

    Board - Layout + App Rotation
    Additional tweaks to the board layout
    Prevent rotation of the views when the device is rotated
    
    Changes to be committed:
    	modified:   Wordle/AppDelegate.swift
    	modified:   Wordle/Board/BoardCell.swift

--- Wordle/Board/BoardCell.swift
+++ Wordle/Board/BoardCell.swift
@@ -14,6 +14,7 @@ class BoardCell: UICollectionViewCell {
     var letter: LetterView = {
         let view = LetterView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.isUserInteractionEnabled = false
         return view
     }()
 
@@ -32,18 +33,9 @@ class BoardCell: UICollectionViewCell {
     //MARK: - Setup and Layout
     private func setupView() {
         addSubview(letter)
-        letter.text = "A"
-
-        backgroundColor = .systemRed
     }
 
     private func setupLayout() {
         letter.addConstraint(topAnchor: topAnchor, leadingAnchor: leadingAnchor, trailingAnchor: trailingAnchor, bottomAnchor: bottomAnchor, paddingTop: 0.0, paddingLeft: 0.0, paddingRight: 0.0, paddingBottom: 0.0, width: 0.0, height: 0.0)
-    }
-
-
-
-    private func setupActions() {
-
     }
 }
